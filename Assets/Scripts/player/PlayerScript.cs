5c04736b1474ad062f0421044aaa1b733fe11ce6
joao <joaoricardoagostinho285@gmail.com>
2025-04-01 22:40:30 -0300

    Added mechanics

--- Assets/Scripts/player/PlayerScript.cs
+++ Assets/Scripts/player/PlayerScript.cs
@@ -5,27 +5,98 @@ public class PlayerScript : MonoBehaviour
     private Rigidbody2D rb;
     private float moveX;
 
-    public float speed;
+    public float speed = 5f;
+    public int maxJumps = 2; // Max number of jumps
+    private int remainingJumps; // Jumps left in the air
+
+    public bool isGrounded;
+    public float jumpForce = 10f;
+
+    private bool jumpQueued = false; // To track the jump button press and queue it up
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        remainingJumps = maxJumps; // Set the remaining jumps at the start
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveX = Input.GetAxisRaw("Horizontal"); // Update moveX with user input
+        // Queue up a jump if the button is pressed
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpQueued = true;
+        }
+        
+        // Update horizontal movement input
+        moveX = Input.GetAxisRaw("Horizontal");
     }
 
     void FixedUpdate()
     {
+        // Apply horizontal movement
         Move();
+        
+
+        // Handle jumping
+        HandleJumping();
     }
 
+    void HandleJumping()
+    {
+        // If we have a queued jump and we're either grounded or have air jumps left
+        if (jumpQueued)
+        {
+            if (isGrounded)
+            {
+                // Reset jump count when grounded
+                remainingJumps = maxJumps - 1; // Subtract 1 because we're using one jump now
+                Jump();
+                
+            }
+            else if (remainingJumps > 0)
+            {
+                // Air jump
+                remainingJumps--;
+                Jump();
+                jumpQueued = false;
+            }
+            jumpQueued = false; 
+        }
+        
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce); // Apply the jump force on the Y-axis
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
     void Move()
     {
-        // Set the new velocity with updated moveX
-        rb.linearVelocity = new Vector2(moveX * speed, rb.linearVelocity.y);
+        rb.velocity = new Vector2(moveX * speed, rb.velocity.y);
+        if(moveX<0)
+        {
+            transform.eulerAngles = new Vector3(0f,180f,0f);
+        }
+        if(moveX>0)
+        {
+            transform.eulerAngles = new Vector3(0f,0f,0f);
+        }
     }
 }
